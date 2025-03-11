d4c8dcd276d3f29bd95319c1a53811196048eb15
phpires <ph_pires@poli.ufrj.br>
2025-03-11 09:08:43 -0300

    Adding CLI interface

--- main.py
+++ main.py
@@ -1,17 +1,23 @@
 from stats import get_num_words, get_characters_count_dict
+import sys
 
-def main():
+def main(book_path):
     print("============ BOOKBOT ============")
-    frankenstein_relative_path = "books/frankenstein.txt"
-    print(f"Analyzing book found at {frankenstein_relative_path}...")
+    print(f"Analyzing book found at {book_path}...")
+
     print("----------- Word Count ----------")
-    num_words = get_num_words(filepath=frankenstein_relative_path)
+    num_words = get_num_words(filepath=book_path)
     print(f"Found {num_words} total words")
+
     print("--------- Character Count -------")
-    char_count_dict = get_characters_count_dict(frankenstein_relative_path)
+    char_count_dict = get_characters_count_dict(book_path)
     for key in char_count_dict.keys():
         print(f"{key}: {char_count_dict[key]}")
     print("============= END ===============")
 
 if __name__ == "__main__":
-    main()
+    if (len(sys.argv)) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    book_path = sys.argv[1]
+    main(book_path)
